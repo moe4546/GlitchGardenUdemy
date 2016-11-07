e3a6d092502f3d4f185034b04a4562f3b739426e
Moritz Horn <horn.moritz@web.de>
2016-11-07 21:04:50 +0100

    Added Splash screen

---
+++ UdemyGlitchGarden/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevelAfter;
+
+	void Start(){
+		Invoke ("LoadNextLevel", autoLoadNextLevelAfter);
+	}
+
+	public void LoadLevel(string name) {		
+		Debug.Log ("Level load requested for: " + name);
+		Application.LoadLevel (name);
+	}
+
+	public void QuitRequest() {
+		Debug.Log ("I Want to quit!");
+		Application.Quit ();
+	}
+
+	public void LoadNextLevel() {
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+}
