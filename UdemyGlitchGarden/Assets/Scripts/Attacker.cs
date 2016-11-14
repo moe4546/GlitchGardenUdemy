0a47f71a43f87959cf8eecf3554ee3a7a1280db4
Moritz Horn <horn.moritz@web.de>
2016-11-14 16:18:24 +0100

    no message

---
+++ UdemyGlitchGarden/Assets/Scripts/Attacker.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attacker : MonoBehaviour {
+
+	[Range (-1f, 1.5f)]
+	public float currentSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.left * currentSpeed * Time.deltaTime, Space.World);
+	}
+
+	void OnTriggerEnter2D(Collider2D name){
+		Debug.Log (name + " trigger enter");
+	}
+
+	public void SetSpeed(float speed) {
+		currentSpeed = speed;
+	}
+
+	public void StrikeCurrentTarget(float damage){
+		Debug.Log ("Hit for " + damage + " dmg");
+	}
+}
